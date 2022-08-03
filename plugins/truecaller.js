4fee5f2b766ea1f716de65194e73ea26cbdd4bae
Hermit-Ser <hermitx39@gmail.com>
2022-08-04 01:30:13 +0530

    fancy, truecaller [addee]

---
+++ plugins/truecaller.js
@@ -0,0 +1,10 @@
+const {Function,truecaller,isPublic} = require('../lib/');
+
+Function({pattern: 'true ?(.*)', fromMe: isPublic, desc: 'search number on truecaller', type: 'misc'}, async (m, match, client) => {
+if(!match && !m.quoted) return await m.reply('_Enter the number you want to search_');
+const number = m.quoted ? m.quoted.sender : match.replace(/[^0-9]/g, '')
+const result = await truecaller(number, m.user_id)
+if (!result) return await m.reply('_Internal Server Busy!_')
+if (result == 500) return await m.reply('_Truecaller limit over!\n(20/20)')
+await m.reply(result);
+})
