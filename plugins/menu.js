e0a00a0f56f057602580e34d326d96a5f704f5f6
Hermit-Ser <hermitx39@gmail.com>
2022-08-04 16:33:32 +0530

    now you can chenage menu owner number

--- plugins/menu.js
+++ plugins/menu.js
@@ -7,6 +7,6 @@ await client.sendMessage(m.chat, {
 image: {url: `${BOT_INFO.split(";")[3]}` },
 caption: `${message.trim()}`,
 footer: `${BOT_INFO.split(";")[0]}`,
-templateButtons: [{urlButton: {displayText: 'Github', url: 'https://github.com/A-d-i-t-h-y-a-n/hermit-md'}},{urlButton: {displayText: 'Contact Owner', url: 'https://wa.me/972528277755'}},{quickReplyButton: {displayText: 'Speed Test', id: 'ping'}},{quickReplyButton: {displayText: 'List Commands', id: 'list'}}]
+templateButtons: [{urlButton: {displayText: 'Github', url: 'https://github.com/A-d-i-t-h-y-a-n/hermit-md'}},{urlButton: {displayText: 'Contact Owner', url: `https://wa.me/${BOT_INFO.split(";")[2]}`}},{quickReplyButton: {displayText: 'Speed Test', id: 'ping'}},{quickReplyButton: {displayText: 'List Commands', id: 'list'}}]
 })
 });
