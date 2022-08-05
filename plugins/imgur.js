58011bb346184897d1b4341718ea543af8f3c5ff
Hermit-Ser <hermitx39@gmail.com>
2022-08-06 01:58:41 +0530

    new audio also supporded in .url

--- plugins/imgur.js
+++ plugins/imgur.js
@@ -1,6 +1,6 @@
-const {Function,Imgur,isPublic} = require("../lib/");
-Function({pattern: 'url ?(.*)', fromMe: isPublic, desc: 'Converts replied media to sticker', type: 'media'}, async (m, text, client) => {
-if (!m.reply_message) return m.reply("_Reply to a video/image!_")
+const {Function,Imgur,isPublic,Vector} = require("../lib/");
+Function({pattern: 'url ?(.*)', fromMe: isPublic, desc: 'upload files to imgur.com', type: 'media'}, async (m, text, client) => {
+if (!m.reply_message) return m.reply("_Reply to a video/image/audio!_")
 if (/image/.test(m.mine)) {
 const media = await client.downloadAndSaveMediaMessage(m.reply_message)
 const url = await Imgur(media)
@@ -9,5 +9,10 @@ await m.reply(url)
 const media = await client.downloadAndSaveMediaMessage(m.reply_message)
 const url = await Imgur(media)
 await m.reply(url)
-} else {return m.reply("_Reply to a video/image!_")
+} else if (/audio/.test(m.mine)) {
+const media = await client.downloadAndSaveMediaMessage(m.reply_message)
+await Vector(media)
+const url = await Imgur('./output.mp4')
+await m.reply(url)
+} else {return m.reply("_Reply to a video/image/audio!_")
 }})
