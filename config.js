ba0bac3dc66d8b6af4bea1561a4399a354ebb5cd
Hermit-Ser <hermitx39@gmail.com>
2022-08-09 00:05:18 +0530

    now you can update

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
+
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 const DATABASE_URL = process.env.DATABASE_URL === undefined ? './database.db' : process.env.DATABASE_URL
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG)
 module.exports = {
