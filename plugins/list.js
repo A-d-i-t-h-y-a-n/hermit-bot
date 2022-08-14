14682921b7b38e890f2df09532dbde0b6532b791
Hermit-Ser <hermitx39@gmail.com>
2022-08-14 21:00:20 +0530

    list added

---
+++ plugins/list.js
@@ -0,0 +1,11 @@
+const {Function,Fancy,commands,isPublic} = require("../lib/");
+Function({pattern: 'list ?(.*)', fromMe: isPublic, dontAddCommandList: true}, async (message, match) => {
+let msg = ''
+let no = 0
+commands.map(async (command) => {
+if (command.dontAddCommandList === false && command.pattern !== undefined) {
+msg += `${no++}. ${command.pattern.toString().match(/(\W*)([A-Za-z0-9_ğüşiö ç]*)/)[2].trim()}\n${command.desc}\n\n`
+}
+})
+await message.reply(await Fancy(msg.trim(), 32))
+})
