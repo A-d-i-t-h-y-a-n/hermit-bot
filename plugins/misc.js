baf80fffb6f0865122760c3022fd770e1599065d
Hermit-Ser <hermitx39@gmail.com>
2022-08-14 21:49:53 +0530

    emix added

--- plugins/misc.js
+++ plugins/misc.js
@@ -1,9 +1,10 @@
 // credit to mask sir
 const {
 	Function,
-	isPublic
+	isPublic,
+	getJson
 } = require("../lib/");
-
+const fs = require('fs');
 Function({
 	pattern: 'readmore ?(.*)',
 	fromMe: isPublic,
@@ -19,6 +20,7 @@ Function({
 	desc: 'wame generator',
 	type: 'misc'
 }, async (m, text, client) => {
+	if (!m.reply_message.sender || m.mention || text) return await m.reply('_Need a Participant_');
 	let sender = 'https://wa.me/' + (m.reply_message.sender || m.mention[0] || text).split('@')[0];
 	await m.reply(sender)
 });
@@ -38,4 +40,27 @@ Function({
 	}, {
 		quoted: m.data
 	})
+})
+
+Function({
+	pattern: 'emix ?(.*)',
+	fromMe: isPublic,
+	desc: 'emoji mix',
+	type: 'search'
+}, async (m, text) => {
+	if (!text) return await m.reply('_Need Emoji!_\n*Example* : 🥸,😁')
+	let [emoji1, emoji2] = text.split(',')
+	if (!emoji1) return await m.reply('_Need 2 Emojis!_\n*Example* : 🥸,😁')
+	if (!emoji2) return await m.reply('_Need 2 Emojis!_\n*Example* : 🥸,😁')
+	const {
+		results
+	} = await getJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
+	for (let res of results) {
+		let media = await m.client.sendImageAsSticker(m.chat, res.url, m.data, {
+			packname: emoji1,
+			author: emoji2,
+			categories: res.tags
+		})
+		await fs.unlinkSync(media)
+	}
 })
