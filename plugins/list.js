26e3f60b0f4a84c0e2d7a513d88511bc263b3fdc
Hermit-Ser <hermitx39@gmail.com>
2022-08-14 21:06:41 +0530

    list fix

--- plugins/list.js
+++ plugins/list.js
@@ -1,7 +1,7 @@
 const {Function,Fancy,commands,isPublic} = require("../lib/");
 Function({pattern: 'list ?(.*)', fromMe: isPublic, dontAddCommandList: true}, async (message, match) => {
 let msg = ''
-let no = 0
+let no = 1
 commands.map(async (command) => {
 if (command.dontAddCommandList === false && command.pattern !== undefined) {
 msg += `${no++}. ${command.pattern.toString().match(/(\W*)([A-Za-z0-9_ğüşiö ç]*)/)[2].trim()}\n${command.desc}\n\n`
