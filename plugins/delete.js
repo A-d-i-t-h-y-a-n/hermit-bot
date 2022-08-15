b9c9cabd7152c73cf8af76d33ff8fe6c8817d700
Hermit-Ser <hermitx39@gmail.com>
2022-08-16 01:38:29 +0530

    .dlt added

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,4 +1,16 @@
 const {Function,isPublic} = require("../lib/");
-Function({pattern: 'delete ?(.*)', fromMe: isPublic, desc: 'delete message that sended by bot', type: 'misc'}, async (m, text, client) => {
+async function isBotAdmins(m, client) {
+const groupMetadata = m.isGroup ? await client.groupMetadata(m.chat).catch(e => {}) : ''
+const participants = m.isGroup ? await groupMetadata.participants : ''
+const groupAdmins = m.isGroup ? await participants.filter(v => v.admin !== null).map(v => v.id) : ''
+return m.isGroup ? groupAdmins.includes(m.user_id) : false
+}
+Function({pattern: 'del ?(.*)', fromMe: isPublic, desc: 'delete message that sended by bot', type: 'misc'}, async (m, text, client) => {
 client.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: true, id: m.quoted.id, participant: m.quoted.sender } })
+})
+Function({pattern: 'dlt ?(.*)', fromMe: isPublic, desc: 'delete message that sended by bot', type: 'misc'}, async (m, text, client) => {
+if (!m.isGroup) return await m.reply('_This command only works in group chats_')
+const isbotAdmin = await isBotAdmins(m, client)
+if (!isbotAdmin) return await m.reply("I'm not an admin")
+await client.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: m.quoted.fromMe, id: m.quoted.id, participant: m.quoted.sender } })
 })
