1e1990fc877bf69c9574007a9650bb697693390e
Hermit-Ser <hermitx39@gmail.com>
2022-08-21 00:57:02 +0530

    Fix

--- plugins/forward.js
+++ plugins/forward.js
@@ -3,10 +3,10 @@ const {Function,parsedJid} = require("../lib/");
 Function({pattern: 'forward ?(.*)', fromMe: true, desc: 'forward replied msg', type: 'misc'}, async (message, match) => {
 if (!message.reply_message) return await message.reply('_Reply to a message_')
 for (let jid of parsedJid(match)) {
-await message.client.forwardMessage(jid, m.quoted_message, {contextInfo: {forwardingScore: 0, isForwarded: false}})}
+await message.client.forwardMessage(jid, m.quoted_message)}
 })
 
 Function({pattern: 'save ?(.*)', fromMe: true, desc: 'forward replied msg to u', type: 'misc'}, async (message, match) => {
 if (!message.reply_message) return await message.reply('_Reply to a message_')
-await message.client.forwardMessage(message.client.user.id, m.quoted_message, {contextInfo: {forwardingScore: 0, isForwarded: false}})
+await message.client.forwardMessage(message.client.user.id, m.quoted_message)
 })
