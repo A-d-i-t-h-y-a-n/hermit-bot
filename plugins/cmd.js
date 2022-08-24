9b8002da3c7d81302e921296fbd368348d6c25f9
Hermit-Ser <hermitx39@gmail.com>
2022-08-24 19:43:04 +0530

    update new!

--- plugins/cmd.js
+++ plugins/cmd.js
@@ -1,9 +1,9 @@
-const {Function,setCmd,delCmd,prepareCmd,getCmdList,isPublic,prefix} = require("../lib/");
+const {Function,setCmd,delCmd,prepareCmd,getCmdList,isPublic} = require("../lib/");
 Function({pattern: 'setcmd ?(.*)', fromMe: true, desc: 'to set audio/image/video as a cmd', type: 'misc'}, async (m, text, client) => {
 if (!m.quoted) return await m.reply('_Reply to a image/video/audio/sticker_')
 if (!text) return await m.reply('_Example : setcmd ping_')
 if (!m.quoted.data.message[m.quoted.mtype].fileSha256) return await m.reply('_Failed_')
-const setcmd = await setCmd(m, prefix + text);
+const setcmd = await setCmd(m, text);
 if (!setcmd) return await m.reply('_Failed_')
 await m.reply('_Success_')
 });
