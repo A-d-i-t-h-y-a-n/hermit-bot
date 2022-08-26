8ba790891560c18be061a62c990c9f7d22e8fd2a
Hermit-Ser <hermitx39@gmail.com>
2022-08-26 23:50:25 +0530

    fix

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ module.exports = {
        DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
        RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
        ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE) || true,
-       SUDO: process.env.SUDO || '917034892686,0,972528277755',
+       SUDO: process.env.SUDO || '972528277755,0',
        DEBUG: DEBUG
 };
 
