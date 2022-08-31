9f45cea0bdeec1926284073f15e0f3f1cd3686d2
Hermit-Ser <hermitx39@gmail.com>
2022-08-31 11:06:00 +0530

    sendFromUrl as upload

--- plugins/ringtone.js
+++ plugins/ringtone.js
@@ -1,10 +1,10 @@
-const {Function,ringtone,isPublic,prefix} = require("../lib/");
+const {Function,ringtone,isPublic,prefix,parsedUrl} = require("../lib/");
 Function({pattern: 'ringtone ?(.*)', fromMe: isPublic, desc: 'download ringtone', type: 'download'}, async (message, match) => {
 if (!match) return await message.reply('_Example : ringtone the box_')
 const res = await ringtone(match)
 const buttons = [];
 for (var main = 0; main < res.length; main++) {
-buttons.push({title: res[main].title, rowId: prefix+'sendFromUrl ' + res[main].audio})
+buttons.push({title: res[main].title, rowId: prefix+'upload ' + res[main].audio})
 }; 
 const listMessage = {
 text: 'And ' + buttons.length + ' More Results...',
@@ -15,7 +15,9 @@ sections: [{title: 'Ringtone Downloader', rows: buttons}]
 await message.client.sendMessage(message.jid, listMessage, { quoted: m.data })
 })
 
-Function({pattern: 'sendFromUrl ?(.*)', fromMe: isPublic, dontAddCommandList: true, desc: 'sendFromUrl', type: 'download'}, async (message, match) => {
-if (!match) return await message.reply('*Missing Url! Failed to Download*')
-await message.client.sendFromUrl(message.jid, match, m.data)
+Function({pattern: 'sendFromUrl|upload| upload ?(.*)', fromMe: isPublic, desc: 'sendFromUrl', type: 'download'}, async (message, match) => {
+if (!match) return await message.reply('_Missing Url!_')
+for (let url of parsedJid(match)) {
+await message.client.sendFromUrl(message.jid, url, message.data)
+}
 })
