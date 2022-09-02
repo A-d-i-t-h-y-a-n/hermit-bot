990a2dbf443cf9309924fe66a3fe18bb5d3d33dd
MASK SER <94510246+mask-sir@users.noreply.github.com>
2022-09-02 23:00:42 +0530

    fix

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 const fs = require('fs');
 const Lang = getString('_plugin');
 
-Function({pattern: 'plugin ?(.*)', fromMe: true, desc: Lang.PLUGIN_DESC, type: 'misc'}, async (m, text) => {
+Function({pattern: 'plugin ?(.*)', fromMe: true, desc: Lang.PLUGIN_DESC, type: 'user'}, async (m, text) => {
 text = text || m.reply_message.text
 await Plugin(text, m)
 });
 
-Function({pattern: 'remove ?(.*)', fromMe: true, desc: Lang.REMOVE_DESC, type: 'misc'}, async (m, text, client) => {
+Function({pattern: 'remove ?(.*)', fromMe: true, desc: Lang.REMOVE_DESC, type: 'user'}, async (m, text, client) => {
 if (text === '') return await m.reply(Lang.NEED_PLUGIN);
 var plugin = await PluginDB.PluginDB.findAll({where: {name: text}});
 if (plugin.length < 1) { return await m.reply(Lang.NOT_FOUND_PLUGIN);} else {await plugin[0].destroy();
