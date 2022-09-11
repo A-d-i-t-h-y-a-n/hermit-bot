62fb902f7fb88f9945fe25629c88b6d7da78e2d1
Hermit-Ser <hermitx39@gmail.com>
2022-09-12 01:04:02 +0530

    implement: remove plugin without restart or reboot

--- plugins/plugin.js
+++ plugins/plugin.js
@@ -1,4 +1,4 @@
-const {Function,getString,PluginDB,Plugin} = require('../lib/');
+const {Function,getString,PluginDB,Plugin,removeCommand} = require('../lib/');
 const Config = require('../config');
 const axios = require('axios');
 const fs = require('fs');
@@ -12,9 +12,16 @@ await Plugin(text, m)
 Function({pattern: 'remove ?(.*)', fromMe: true, desc: Lang.REMOVE_DESC, type: 'user'}, async (m, text, client) => {
 if (text === '') return await m.reply(Lang.NEED_PLUGIN);
 var plugin = await PluginDB.PluginDB.findAll({where: {name: text}});
-if (plugin.length < 1) { return await m.reply(Lang.NOT_FOUND_PLUGIN);} else {await plugin[0].destroy();
+if (plugin.length < 1) { return await m.reply(Lang.NOT_FOUND_PLUGIN);} else {
+const got = require('got')
+const res = await got(plugin[0].dataValues.url)
+if (res.statusCode == 200) {
+let name = /pattern: ["'](.*)["'],/g.exec(res.body)
+name = name[1].split(' ')[0]
+await removeCommand(name)
+}
+await plugin[0].destroy();
 delete require.cache[require.resolve('./' + text + '.js')]
 fs.unlinkSync('./plugins/' + text + '.js');
 await m.reply(Lang.DELETED.replace('{}', text));}
-require('pm2').restart('index.js');
 });
