f13b1460f661dbfe1fd1ac22c663cea6f0086d59
Hermit-Ser <hermitx39@gmail.com>
2022-09-15 20:41:17 +0530

    fix

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,12 +1,47 @@
-const {Function,isPublic,Menu,RandomFancy,prefix} = require('../lib/');
-const {BOT_INFO} = require('../config')
+const {
+	Function,
+	isPublic,
+	Menu,
+	RandomFancy,
+	prefix,
+	parsedUrl
+} = require('../lib/');
+const {
+	BOT_INFO
+} = require('../config')
 
-Function({pattern: 'menu ?(.*)', fromMe: isPublic, dontAddCommandList: false, type: 'info'}, async (m, text, client) => {
-const message= await RandomFancy(await Menu(m))
-await client.sendMessage(m.chat, {
-image: {url: `${BOT_INFO.split(";")[3]}` },
-caption: `${message}`,
-footer: `${BOT_INFO.split(";")[0]}`,
-templateButtons: [{quickReplyButton: {displayText: 'Speed Test', id: prefix + 'ping'}},{quickReplyButton: {displayText: 'List Commands', id: prefix + 'list'}}]
-})
+Function({
+	pattern: 'menu ?(.*)',
+	fromMe: isPublic,
+	type: 'info'
+}, async (message, match, client) => {
+	const menuMessage = await Menu(message)
+	const msg = await RandomFancy(menuMessage)
+	var img = await parsedUrl(BOT_INFO)
+	img = img || ['https://i.imgur.com/qJUBCYm.jpeg']
+	const image = img[Math.floor(Math.random() * img.length)]
+	const type = image.endsWith('mp4') ? 'video' : 'image'
+	const buttonMessage = {
+		[type]: {
+			url: img
+		},
+		caption: `${msg}`,
+		footer: `${BOT_INFO.split(";")[0] || ' '}`,
+		buttons: [{
+				buttonId: prefix + 'ping',
+				buttonText: {
+					displayText: 'Speed Test'
+				},
+				type: 1
+			},
+			{
+				buttonId: prefix + 'list',
+				buttonText: {
+					displayText: 'List Commands'
+				},
+				type: 1
+			}
+		]
+	}
+	await message.client.sendMessage(message.chat, buttonMessage)
 });
