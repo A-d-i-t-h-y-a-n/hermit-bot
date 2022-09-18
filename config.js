abfaac459fde37ac29fa5acee680a686d0af3b82
Hermit-Ser <hermitx39@gmail.com>
2022-09-19 01:55:37 +0530

    fect: it was my mistake

--- config.js
+++ config.js
@@ -41,7 +41,6 @@ module.exports = {
        DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
        RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
        BRAIN_ID: process.env.BRAIN_ID === undefined ? false : process.env.BRAIN_ID,
-       ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE) || true,
        SUDO: process.env.SUDO || '972528277755,0',
        DEBUG: DEBUG
 };
