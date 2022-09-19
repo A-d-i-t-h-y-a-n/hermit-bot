74999ca212b03d48c48726d92ba6c834bc7f35c2
Hermit-Ser <hermitx39@gmail.com>
2022-09-19 17:59:24 +0530

    update: app.json

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,15 +1,20 @@
-const {Function,isPublic,sendAlive,RandomFancy} = require("../lib/");
+const { Function, isPublic, sendAlive, RandomFancy, runtime } = require("../lib/");
 const config = require('../config')
-Function({pattern: 'ping ?(.*)', fromMe: isPublic, desc: 'Bot response in second.', type: 'info'}, async (m, text, client) => {
+Function({pattern: 'ping ?(.*)', fromMe: isPublic, desc: 'Bot response in second.', type: 'info'}, async (message, match, client) => {
   var start = new Date().getTime();
-  var msg = await m.reply('*Testing Speed..*');
+  var msg = await message.reply('*Testing Speed..*');
   var end = new Date().getTime();
-  await m.reply('⟪ *Response in ' + (end - start) + ' msec* ⟫');
+  await message.reply('⟪ *Response in ' + (end - start) + ' msec* ⟫');
 });
 
-Function({pattern: 'alive ?(.*)', fromMe: isPublic, desc: 'Does bot work?', type: 'info'}, async (m, text, client) => {
-  await sendAlive(client, m, text);
+Function({pattern: 'alive ?(.*)', fromMe: isPublic, desc: 'Does bot work?', type: 'info'}, async (message, match, client) => {
+  await sendAlive(client, message, match);
 });
-Function({pattern: 'jid ?(.*)', fromMe: isPublic, desc: 'to get remoteJid', type: 'whatsapp'}, async (m) => {
-  await m.reply(m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.chat)
+
+Function({pattern: 'jid', fromMe: isPublic, desc: 'to get remoteJid', type: 'whatsapp'}, async (message => {
+  await message.reply(message.mentionedJid[0] ? message.mentionedJid[0] : message.quoted ? message.quoted.sender : message.chat)
+});
+
+Function({pattern: 'runtime', fromMe: isPublic, desc: 'get bots runtime', type: 'info'}, async (message, match, client) => {
+  await message.send(await runtime(process.uptime()));
 });
