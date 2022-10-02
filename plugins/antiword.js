51123320a408f81a6ad519b565c235fa9861b5a1
Hermit-Ser <hermitx39@gmail.com>
2022-10-03 02:04:45 +0530

    AntiLink added

--- plugins/antiword.js
+++ plugins/antiword.js
@@ -1,5 +1,9 @@
-const { Function, antiWord } = require('../lib/');
+const { Function, antiWord, antiLink } = require('../lib/');
 
 Function({pattern: 'antiword ?(.*)', fromMe: true, desc: 'set antiword', type: 'group'}, async (message, match) => {
 await antiWord(message, match, client)
+})
+
+Function({pattern: 'antilink ?(.*)', fromMe: true, desc: 'set antilink', type: 'group'}, async (message, match, client) => {
+await antiLink(message, match, client)
 })
