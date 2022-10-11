0881aecfdf29b40194da06c0a8f71b45c916bbe7
Hermit-Ser <hermitx39@gmail.com>
2022-10-12 01:57:13 +0530

    SONG,VIDEO,YTA,YTV FiX

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -8,6 +8,7 @@ const {
 	isPublic,
 	yta,
 	ytv,
+	yt,
 	ytIdRegex,
 	getJson
 } = require('../lib/');
@@ -28,7 +29,7 @@ Function({
 		let ytId = ytIdRegex.exec(match)
 		let media
 		try {
-			media = await yta('https://youtu.be/' + ytId[1], '128kbps')
+			media = await yt('https://youtu.be/' + ytId[1], '128kbps', 'mp3', '128', 'en412')
 		} catch (error) {
 			media = await getJson(apiUrl + 'api/yta/' + ytId[1])
 		}
@@ -82,22 +83,44 @@ Function({
 	type: 'download'
 }, async (message, match, client) => {
 	match = match || message.reply_message.text
-	if (!match) return message.reply('_Need youtube video link!_')
-	match = match.match(new RegExp(/(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed|shorts)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]+)/, 'gi'))
-	if (!match) return message.reply('_Need youtube video link!_')
-	let ytId = ytIdRegex.exec(match)
-	let quality = message.match[1] ? message.match[1] : '360p'
-	let media
-	try {
-		media = await ytv('https://youtu.be/' + ytId[1], quality)
-	} catch (error) {
-		media = await getJson(apiUrl + 'api/ytv/' + ytId[1] + '?quality=' + quality)
+	if (!match) return message.reply(Lang.NEED_TEXT_SONG)
+	if (isUrl(match) && match.includes('youtu')) {
+		let ytId = ytIdRegex.exec(match)
+		let media
+		try {
+			result = await yt('https://youtu.be/' + ytId[1], '360p', 'mp4', '360', 'en412')
+		} catch (error) {
+			media = await getJson(apiUrl + 'api/ytv/' + ytId[1])
+		}
+		await message.client.sendMessage(message.chat, {
+			video: await getBuffer(media.dl_link)
+		}, {
+			quoted: message.data
+		})
+		return;
 	}
-	if (media.filesize >= 100000) return message.reply('_Unable to download video_')
-	await client.sendMessage(message.chat, {
-		video: await getBuffer(media.dl_link),
-		mimetype: 'video/mp4',
-		caption: media.title
+	let search = await yts(match)
+	if (search.all.length < 1) return await message.reply(Lang.NO_RESULT);
+	let listbutton = [];
+	let no = 1;
+	for (var z of search.videos) {
+		let button = {
+			title: 'Result - ' + no++ + ' ',
+			rows: [{
+				title: z.title,
+				rowId: prefix + 'video ' + z.url
+			}]
+		};
+		listbutton.push(button)
+	};
+	const listMessage = {
+		text: `And ${listbutton.length} More Results...`,
+		title: search.videos[0].title,
+		buttonText: 'Select video',
+		sections: listbutton
+	}
+	await client.sendMessage(message.chat, listMessage, {
+		quoted: message.data
 	})
 });
 
@@ -113,7 +136,7 @@ Function({
 		const ytId = ytIdRegex.exec(match)
 		let result
 		try {
-			result = await yta('https://youtu.be/' + ytId[1], '128kbps')
+			result = await yt('https://youtu.be/' + ytId[1], '128kbps', 'mp3', '128', 'en412')
 		} catch (error) {
 			result = await getJson(apiUrl + 'api/yta/' + ytId[1])
 		}
@@ -134,7 +157,12 @@ Function({
 	}
 	const search = await yts(match)
 	if (search.all.length < 1) return await message.reply('_Not Found_');
-	const result = await yta(search.videos[0].url, '128kbps')
+	let result
+	try {
+		result = await yt('https://youtu.be/' + search.videos[0].videoId, '128kbps', 'mp3', '128', 'en412')
+	} catch (error) {
+		result = await getJson(apiUrl + 'api/yta/' + search.videos[0].videoId)
+	}
 	const thumbnail = await getBuffer(result.thumb)
 	if (result.filesize >= 10000) return await message.client.sendMessage(message.jid, {
 		audio: await getBuffer(result.dl_link),
@@ -165,11 +193,13 @@ Function({
 		if (quality) {
 			quality = quality[1]
 		}
+		const resol = quality || '360p'
 		let result
 		try {
-			result = await ytv('https://youtu.be/' + ytId[1], quality || '360p')
+			result = await yt('https://youtu.be/' + ytId[1], resol, 'mp4', resol.endsWith('p') ? resol.replace(/p/g, '') : resol, 'en412')
 		} catch (error) {
-			result = await getJson(apiUrl + 'api/ytv/' + ytId[1] + '?quality=' + quality)
+			console.log(apiUrl + 'api/ytv/' + ytId[1] + '?quality=' + resol)
+			result = await getJson(apiUrl + 'api/ytv/' + ytId[1] + '?quality=' + resol)
 		}
 		if (result.filesize > 100000) {
 			const url = await getJson('https://tinyurl.com/api-create.php?url=' + result.dl_link)
@@ -189,32 +219,32 @@ Function({
 			title: result.title,
 			rows: [{
 					title: '1080p',
-					description: result.list['1080p'],
+					description: result.list['1080p'] || 'Not Available',
 					rowId: `${prefix}ytv https://youtu.be/${ytId[1]} {1080p}`
 				},
 				{
 					title: '720p',
-					description: result.list['720p'],
+					description: result.list['720p'] || 'Not Available',
 					rowId: `${prefix}ytv https://youtu.be/${ytId[1]} {720p}`
 				},
 				{
 					title: '480p',
-					description: result.list['480p'],
+					description: result.list['480p'] || 'Not Available',
 					rowId: `${prefix}ytv https://youtu.be/${ytId[1]} {480p}`
 				},
 				{
 					title: '360p',
-					description: result.list['360p'],
+					description: result.list['360p'] || 'Not Available',
 					rowId: `${prefix}ytv https://youtu.be/${ytId[1]} {360p}`
 				},
 				{
 					title: '240p',
-					description: result.list['240p'],
+					description: result.list['240p'] || 'Not Available',
 					rowId: `${prefix}ytv https://youtu.be/${ytId[1]} {240p}`
 				},
 				{
 					title: '144p',
-					description: result.list['144p'],
+					description: result.list['144p'] || 'Not Available',
 					rowId: `${prefix}ytv https://youtu.be/${ytId[1]} {144p}`
 				}
 			]
@@ -229,7 +259,12 @@ Function({
 	}
 	const search = await yts(match)
 	if (search.all.length < 1) return await message.reply('_Not Found_');
-	const result = await ytv(search.videos[0].url, '360p')
+	let result
+	try {
+		result = await yt('https://youtu.be/' + search.videos[0].videoId, '360p', 'mp4', '360', 'en412')
+	} catch (error) {
+		result = await getJson(apiUrl + 'api/ytv/' + search.videos[0].videoId)
+	}
 	if (result.filesize > 100000) {
 		const url = await getJson('https://tinyurl.com/api-create.php?url=' + result.dl_link)
 		return await message.reply('*Failed to Download*\n_File size is more is than 100MB_\nClick this url to download manually : ' + url)
