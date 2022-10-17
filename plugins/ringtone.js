2c9bbc1a16d324dc6d4170ae7a481dc50d9cff13
Hermit-Ser <hermitx39@gmail.com>
2022-10-17 18:27:07 +0530

    format code

--- plugins/ringtone.js
+++ plugins/ringtone.js
@@ -1,25 +1,49 @@
-const {Function,ringtone,isPublic,prefix,parsedUrl} = require("../lib/");
-Function({pattern: 'ringtone ?(.*)', fromMe: isPublic, desc: 'download ringtone', type: 'download'}, async (message, match) => {
-if (!match) return await message.reply('_Example : ringtone the box_')
-const res = await ringtone(match)
-const buttons = [];
-for (var main = 0; main < res.length; main++) {
-buttons.push({title: res[main].title, rowId: prefix+'upload ' + res[main].audio})
-}; 
-const listMessage = {
-text: 'And ' + buttons.length + ' More Results...',
-title: res[0].title,
-buttonText: 'Select Ringtone',
-sections: [{title: 'Ringtone Downloader', rows: buttons}]
-};
-await message.client.sendMessage(message.jid, listMessage, { quoted: m.data })
+const {
+	Function,
+	ringtone,
+	isPublic,
+	prefix,
+	parsedUrl
+} = require("../lib/");
+Function({
+	pattern: 'ringtone ?(.*)',
+	fromMe: isPublic,
+	desc: 'download ringtone',
+	type: 'download'
+}, async (message, match) => {
+	if (!match) return await message.reply('_Example : ringtone the box_')
+	const res = await ringtone(match)
+	const buttons = [];
+	for (var main = 0; main < res.length; main++) {
+		buttons.push({
+			title: res[main].title,
+			rowId: prefix + 'upload ' + res[main].audio
+		})
+	};
+	const listMessage = {
+		text: 'And ' + buttons.length + ' More Results...',
+		title: res[0].title,
+		buttonText: 'Select Ringtone',
+		sections: [{
+			title: 'Ringtone Downloader',
+			rows: buttons
+		}]
+	};
+	await message.client.sendMessage(message.jid, listMessage, {
+		quoted: m.data
+	})
 })
 
-Function({pattern: 'upload ?(.*)', fromMe: isPublic, desc: 'sendFromUrl', type: 'download'}, async (message, match) => {
-match = match || message.reply_message.text
-if (!match) return await message.reply('_Missing Url!_')
-const Url = await parsedUrl(match)
-for (let url of Url) {
-await message.client.sendFromUrl(message.jid, url, message.data)
-}
+Function({
+	pattern: 'upload ?(.*)',
+	fromMe: isPublic,
+	desc: 'sendFromUrl',
+	type: 'download'
+}, async (message, match) => {
+	match = match || message.reply_message.text
+	if (!match) return await message.reply('_Missing Url!_')
+	const Url = await parsedUrl(match)
+	for (let url of Url) {
+		await message.client.sendFromUrl(message.jid, url, message.data)
+	}
 })
--- plugins/truecaller.js
+++ plugins/truecaller.js
@@ -1,10 +1,23 @@
-const {Function,truecaller,isPublic} = require('../lib/');
+const {
+	Function,
+	truecaller,
+	isPublic
+} = require('../lib/');
 
-Function({pattern: 'true ?(.*)', fromMe: isPublic, desc: 'search number on truecaller', type: 'search'}, async (m, match, client) => {
-if (!match && !m.quoted) return await m.reply('_Enter the number you want to search_');
-const number = m.quoted ? m.quoted.sender : match.replace(/[^0-9]/g, '')
-const result = await truecaller(number, m.user_id)
-if (!result) return await m.reply('_Internal Server Busy!_')
-if (result == 500) return await m.reply('_Truecaller limit over!\n(20/20)')
-await m.reply(result);
+Function({
+	pattern: 'true ?(.*)',
+	fromMe: isPublic,
+	desc: 'search number on truecaller',
+	type: 'search'
+}, async (m, match, client) => {
+	try {
+		if (!match && !m.quoted) return await m.reply('_Enter the number you want to search_');
+		const number = m.quoted ? m.quoted.sender : match.replace(/[^0-9]/g, '')
+		const result = await truecaller(number, m.user_id)
+		if (!result) return await m.reply('_Internal Server Busy!_')
+		if (result == 500) return await m.reply('_Truecaller limit over!\n(20/20)')
+		await m.reply(result);
+	} catch (error) {
+		await m.reply("_Daily limit over_")
+	}
 })
