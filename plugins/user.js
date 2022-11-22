5df9698eeb758a2bfd189f3cfc15bb73212e9909
Hermit-Ser <hermitx39@gmail.com>
2022-11-22 16:45:31 +0530

    fix..

--- plugins/user.js
+++ plugins/user.js
@@ -153,9 +153,9 @@ Function({
 	type: 'whatsapp'
 }, async (message, match) => {
 match = match || message.reply_message.text
-if (!match) return await message.send('*Need Number!*\n*Example: onwa 123456*')
+if (!match) return await message.send('*Need Number!*\n*Example: onwa 123456789*')
 match = match.replace(/[^0-9]/g, '')
-if (!match) return await message.send('*Need Number!*\n*Example: onwa 123456*')
+if (!match) return await message.send('*Need Number!*\n*Example: onwa 123456789*')
 const [result] = await message.client.onWhatsApp(match)
 if (!result) await message.send(match + " doest exists on WhatsApp")
 if (result && result.exists) {
