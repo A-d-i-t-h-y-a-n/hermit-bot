c6229c009a0f74a48052cfc590d15ea104ec77ed
Hermit-Ser <hermitx39@gmail.com>
2022-11-23 12:23:11 +0530

    feat: PollCreationMessage WAProto bug fix

--- plugins/poll.js
+++ plugins/poll.js
@@ -1,4 +1,6 @@
-const { Function, poll } = require('../lib/')
+const { Function } = require('../lib/')
+const { randomBytes } = require('crypto')
+const { WAProto } = require('@adiwajshing/baileys')
 Function({
 	pattern: 'poll ?(.*)',
 	fromMe: true,
@@ -6,5 +8,24 @@ Function({
 	type: 'whatsapp'
 }, async (message, match) => {
 if (!match) return await message.send('_Need options!_\n*Example: poll head,option1,option2,option3....*')
-await poll(message, match)
+const m = {}
+match = match.split(',')
+const buttons = []
+for (let i = 1; i < match.length; i++) {
+buttons.push(match[i])
+}
+const arr = randomBytes(32)
+m.messageContextInfo = {
+messageSecret: arr
+}
+m.pollCreationMessage = WAProto.Message.PollCreationMessage.fromObject({
+name: match[0],
+selectableOptionsCount: 0,
+options: buttons.map(
+value => WAProto.Message.PollCreationMessage.Option.fromObject({
+optionName: value
+}),
+)
+})
+await message.client.relayMessage(message.jid, m, {});
 })
