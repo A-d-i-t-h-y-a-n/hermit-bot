caf66b82325d3e4f12982c37dd96aa75a1fd9f2a
Hermit-Ser <hermitx39@gmail.com>
2023-05-08 15:07:32 +0530

    fix

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -33,7 +33,7 @@ Function({
 	if (isUrl(match) && match.includes('youtu')) {
 		let ytId = ytIdRegex.exec(match)
 		const media = await downloadYouTubeAudio(ytId[1])
-		if (media.content_length >= 10000) return await send(message, await fs.readFileSync(media.file), ytId[1])
+		if (media.bitrate >= 10000) return await send(message, await fs.readFileSync(media.file), ytId[1])
 		let thumb = await getBuffer(media.thumb)
 		let writer = await addAudioMetaData(await fs.readFileSync(media.file), thumb, media.title, `${config.BOT_INFO.split(";")[0]}`, 'Hermit Official')
 		await send(message, writer, ytId[1])
