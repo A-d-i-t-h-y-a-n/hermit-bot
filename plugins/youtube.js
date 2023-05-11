fabe19025dc1f67ac2a5be00960fd3ff64c12f1f
Hermit-Ser <hermitx39@gmail.com>
2023-05-11 22:18:21 +0530

    fix: pdf in order

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -119,7 +119,7 @@ Function({
 	if (search.all.length < 1) return await message.reply('_Not Found_');
 	const result = await downloadYouTubeAudio(search.videos[0].videoId)
 	const thumbnail = await getBuffer(result.thumb)
-	if (result.bitrate >= 10000) return await message.client.sendMessage(message.jid, {audio: await fs.readFileSync(media.file), mimetype: 'audio/mpeg'}, {quoted: message.data})
+	if (result.bitrate >= 10000) return await message.client.sendMessage(message.jid, {audio: await fs.readFileSync(result.file), mimetype: 'audio/mpeg'}, {quoted: message.data})
 	const file = await addAudioMetaData(await fs.readFileSync(result.file), thumbnail, result.title, `${config.BOT_INFO.split(";")[0]}`, 'Hermit Official')
 	return await message.client.sendMessage(message.jid, {audio: await fs.readFileSync(result.file), mimetype: 'audio/mpeg'}, {quoted: message.data})
 });
