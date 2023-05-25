0a897254a61c3b0e45d26fe26117951459a006c9
Hermit-Ser <hermitx39@gmail.com>
2023-05-25 18:14:14 +0530

    files  ആഡ് ച്ചയാണ് മറന്നുപോയി

---
+++ plugins/msgs.js
@@ -0,0 +1,30 @@
+const { Function, getMessage } = require('../lib/');
+const { runtime } = require('../menu');
+
+Function({
+	pattern: 'msgs ?(.*)',
+	fromMe: true,
+	desc: 'Gives an overview of the total messages in the group',
+	type: 'group'
+}, async (message, match, client) => {
+  if (!message.isGroup) return await message.reply('_This command only works in group chats_')
+  const userId = message.mention[0] || message.reply_message?.sender;
+  const data = await getMessage(message.jid);
+  const timeNow = new Date().getTime();
+  let msg = '';
+  if (userId) {
+    const user = await getMessage(message.jid, userId);
+    msg += '*Number :* ' + userId.split("@")[0] + '\n*Name :* ' + (user.name || 'Unknown') + '\n';
+    Object.keys(user.type).map(item => msg += '*' + item + ' :* ' + user.type[item] + '\n');
+    msg += '*Total :* ' + user.total + '\n';
+    msg += '*lastActivity :*' + runtime((timeNow - user.time) / 1000) + ' ago\n\n';
+  } else {
+    Object.keys(data).map(user => {
+      const { name, total, type, time } = data[user];
+      msg += '*Number :* ' + user.split("@")[0] + '\n*Name :* ' + (name || 'Unknown') + '\n*Total Msgs :* ' + total + '\n';
+      Object.keys(type).map(item => msg += '*' + item + ' :* ' + type[item] + '\n');
+      msg += '*lastActivity :* ' + runtime((timeNow - time) / 1000) + ' ago\n\n';
+    });
+  }
+  return await message.send(msg.trim());
+})
