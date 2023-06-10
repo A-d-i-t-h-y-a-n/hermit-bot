0b497b473e8e80fee1227225e716827f84572090
Hermit-Ser <hermitx39@gmail.com>
2023-06-10 15:06:03 +0530

    try catch && check type before declare

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -35,11 +35,16 @@ Function({
   if (!urls) return await message.send('*The replied message does not contain any YouTube search results.*');
   if (isNaN(index) || index < 1 || index > urls.length) return await message.send('*Invalid index.*\n_Please provide a number within the range of search results._');
   let ytId = ytIdRegex.exec(urls[index - 1]);
+  try {
   const media = await downloadYouTubeAudio(ytId[1]);
   if (media.content_length >= 10485760) return await send(message, await fs.readFileSync(media.file), ytId[1]);
   const thumb = await getBuffer(media.thumb);
   const writer = await addAudioMetaData(await toAudio(await fs.readFileSync(media.file)), thumb, media.title, `hermit-md`, 'Hermit Official');
   return await send(message, writer, ytId[1]);
+  } catch {
+  const response = await getJson('https://api.adithyan.ml/ytaudio?id=' + ytId[1]);
+  if (response.status) return await client.sendMessage(message.jid, { audio: {url: response.result }, mimetype: 'audio/mpeg', ptt: false }, { quoted: message.data });
+  }
   } else if (text.includes('Search results') && text.includes('Format: video')) {
   const urls = message.reply_message.text.match(ytRegex);
   if (!urls) return await message.send('*The replied message does not contain any YouTube search results.*');
@@ -76,18 +81,25 @@ Function({
 	if (!match) return message.reply(Lang.NEED_TEXT_SONG)
 	if (isUrl(match) && match.includes('youtu')) {
 		let ytId = ytIdRegex.exec(match)
-		const media = await downloadYouTubeAudio(ytId[1])
+		try {
+		const media = await downloadYouTubeAudio()
 		if (media.content_length >= 10485760) return await send(message, await fs.readFileSync(media.file), ytId[1])
 		const thumb = await getBuffer(await getYoutubeThumbnail(ytId[1]))
 		const writer = await addAudioMetaData(await toAudio(await fs.readFileSync(media.file)), thumb, media.title, `${config.BOT_INFO.split(";")[0]}`, 'Hermit Official')
 		return await send(message, writer, ytId[1])
+		} catch {
+			const response = await getJson('https://api.adithyan.ml/ytaudio?id=' + ytId[1])
+			if (response.status) return await client.sendMessage(message.jid, { audio: {url: response.result }, mimetype: 'audio/mpeg', ptt: false }, { quoted: message.data })
+	   }
 	}
 	const search = await yts(match)
 	if (search.all.length < 1) return await message.reply(Lang.NO_RESULT);
 	let no = 1;
 	let listText = `${t}Search results for ${match}:${t}\n\n*Format: audio*\n\n`;
 	for (let i of search.all) {
+	if (i.type == 'video') {
     listText += `${no++}. *${i.title}*\nhttps://youtu.be/${i.url.match(/(?<=\?v=)[^&]+/)[0]}\n\n`;
+    }
     }
     await message.send(listText);
     /* 
@@ -126,7 +138,9 @@ Function({
 	let no = 1;
 	let listText = `${t}Search results for ${match}:${t}\n\n*Format: video*\n\n`;
 	for (let i of search.all) {
+	if (i.type == 'video') {
     listText += `${no++}. *${i.title}*\nhttps://youtu.be/${i.url.match(/(?<=\?v=)[^&]+/)[0]}\n\n`;
+    }
     }
     await message.send(listText);
 	/* const listbutton = [];
@@ -180,7 +194,9 @@ Function({
 	let list = '';
 	let no = 1;
 	for (i of result.video) {
+	if (i.type == 'video') {
 	list += `${no++}. ${i.fquality} - ${i.filesize}\n`;
+	}
 	}
 	return await message.send(`*${result.title}*\n\n*id: ${ytId[1]}*\n\n${t}Available quality${t}\n\n${list}\n_To download, please reply with the desired quality number._`);
 	};
