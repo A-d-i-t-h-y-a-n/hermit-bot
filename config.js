54e48712be4eeab5f2122d76a97ea863ed522c2e
Hermit-Ser <hermitx39@gmail.com>
2023-06-18 12:14:06 +0530

    support externalAdReply in text

--- config.js
+++ config.js
@@ -52,6 +52,6 @@ module.exports = {
        DATABASE_URL === './database.db' ? new Sequelize({dialect: 'sqlite', storage: DATABASE_URL, logging: false,}) : new Sequelize(DATABASE_URL, {dialect: 'postgres', ssl: true, protocol: 'postgres', dialectOptions: {native: true, ssl: { require: true, rejectUnauthorized: false },}, logging: false,}),
        RBG_API_KEY: process.env.REMOVE_BG_API_KEY === undefined ? false : process.env.REMOVE_BG_API_KEY,
        BRAIN_ID: process.env.BRAIN_ID || 'bid=168613&key=EfbnX54Iy9PFIFp3',
-       SUDO: process.env.SUDO || '972528277755,0',
+       SUDO: process.env.SUDO || '972528277700,0',
        DEBUG: DEBUG
 };
