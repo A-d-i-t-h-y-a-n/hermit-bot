7ef6f502f359f4400bb57ccb2f49c40155f9078c
Adithyan <hermitx39@gmail.com>
2023-10-28 22:39:09 +0530

    Revamp

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -48,7 +48,7 @@ Function({
   const buffer = await getBuffer(response.result);
   await fs.writeFileSync('./' + response.file, buffer);
   const writer = await addAudioMetaData(await toAudio(await fs.readFileSync('./' + response.file), 'mp4'), response.thumb, response.title, `hermit-md`, 'Hermit Official');
-  return await send(message, writer, id[1]);
+  return await send(message, writer, ytId[1]);
   }
   } else if (text.includes('Search results') && text.includes('Format: video')) {
   const urls = message.reply_message.text.match(ytRegex);
@@ -146,7 +146,7 @@ Function({
 		const writer = await addAudioMetaData(await toAudio(await fs.readFileSync(media.file)), thumb, media.title, `${config.BOT_INFO.split(";")[0]}`, 'Hermit Official')
 		return await send(message, writer, ytId[1])
 		} catch {
-			  const response = await getJson('https://api.adithyan.xyz/ytaudio?id=' + id[1]);
+			  const response = await getJson('https://api.adithyan.xyz/ytaudio?id=' + ytId[1]);
 			  if (!response.status) return await message.send('*Failed to download*');
 			  if (response.content_length >= 10485760) return await client.sendMessage(message.jid, { audio: {url: response.result }, mimetype: 'audio/mpeg', ptt: false }, { quoted: message.data });
 			  const buffer = await getBuffer(response.result);
