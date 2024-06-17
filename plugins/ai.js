b406104abe723ce883030ae6369a6de713f33b5d
Adithyan <hermitx39@gmail.com>
2024-06-17 20:11:20 +0530

    bing added

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,6 +1,7 @@
 const {
 	Function,
-	dalle
+	dalle,
+	bing
 } = require("../lib/");
 
 Function({
@@ -13,4 +14,16 @@ Function({
 	await message.send('*Generating image...*\n_It may take a while_')
 	const image = await dalle(match)
 	await message.send(image, 'image')
+})
+
+Function({
+	pattern: 'bing ?(.*)',
+	fromMe: true,
+	desc: 'Microsoft bing ai',
+	type: 'ai'
+}, async (message, match) => {
+	if (!match) return await message.reply('*Need a prompt!*\n_Example: dalle starry sky over the city_')
+	const msg = await message.send('_Thinking..._')
+	const content = await bing(match)
+	await msg.edit(content)
 })
