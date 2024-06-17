6512e35c56af40fd9014e36be2e30f1219c1805a
Adithyan <hermitx39@gmail.com>
2024-06-17 16:50:54 +0530

    error msg default value true

--- config.js
+++ config.js
@@ -32,7 +32,7 @@ module.exports = {
     BOT_INFO: process.env.BOT_INFO || 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ;972528277755;https://i.imgur.com/6oRG106.jpeg',
     AUDIO_DATA: process.env.AUDIO_DATA === undefined ? 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ;https://i.imgur.com/fj2WE83.jpeg' : process.env.AUDIO_DATA,
     STICKER_DATA: process.env.STICKER_DATA === undefined ? 'ʜᴇʀᴍɪᴛ;ᴀᴅɪᴛʜyᴀɴ' : process.env.STICKER_DATA,
-    ERROR_MESSAGE: toBool(process.env.ERROR_MESSAGE), 
+    ERROR_MESSAGE: process.env.ERROR_MESSAGE === undefined ? true : toBool(process.env.ERROR_MESSAGE), 
     SONG_THUMBNAIL: toBool(process.env.SONG_THUMBNAIL),
     WARN: process.env.WARN || '4',
     EXPRESS: toBool(process.env.EXPRESS),
