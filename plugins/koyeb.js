e4757b562af1006205168c0807c3bd93273e3e98
Adithyan <hermitx39@gmail.com>
2024-06-27 18:55:53 +0530

    fix

--- plugins/koyeb.js
+++ plugins/koyeb.js
@@ -1,4 +1,4 @@
-const { Function } = require('../lib/');
+const { Function, prefix } = require('../lib/');
 const config = require('../config')
 const simpleGit = require('simple-git');
 if (config.KOYEB_API_KEY) {
@@ -21,50 +21,64 @@ pm2.connect(function(err) {
         if (err) throw err;
     });
 });
-    
+
 Function({
     pattern: 'update ?(.*)',
     fromMe: true,
     desc: 'Update bot from GitHub repository',
     type: 'koyeb'
 }, async (message, match, client) => {
-    if (match && match.toLowerCase() === 'check') {
+	if (!match || (match.toLowerCase() == 'check')) {
+        await git.fetch();
+        const commits = await git.log(['main..origin/main']);
+        let msg = ''; let no = 1;
+        commits.all.map((commit) => {
+        	msg += '' + no++ + '. ' + commit.message + '\n';
+        });
+        if (commits.total > 0) {
+            const interactiveMessage = {
+                title: 'New update available!\n\nChanges:',
+                text: msg,
+                footer: 'hermit-md',
+                subtitle: 'hermit-md',
+                buttons: [{
+                    type: 'button',
+                    display_text: 'Update Now',
+                    id: prefix + 'update now'
+                }]
+            };
+            
+            return await client.interactiveMessage(message.jid, interactiveMessage);
+        } else {
+            return await message.send('_Bot is completely up-to-date!_');
+        }
+     }
+
+    if (match && (match.toLowerCase() === 'now' || match.toLowerCase() === 'start')) {
         try {
-            await git.fetch();
-            const commits = await git.log(['main' + '..origin/' + 'main']);
-            if (commits.total > 0) {
-                await message.send('_New updates are available!_');
-            } else {
-                await message.send('_No new updates available._');
-            }
+            await message.send('_Build started_')
+            let intervalId;
+            intervalId = setInterval(async function() {
+            	const { deployments } = await koyeb.getDeployments(config.KOYEB_APP_NAME)
+                if (deployments[0].status == 'CANCELED') {
+                	await message.reply('*Deployment Canceled*')
+					clearInterval(intervalId);
+                } else if (deployments[0].status == 'STOPPED') {
+                	await message.reply('*Deployment Stopped*')
+	                clearInterval(intervalId);
+				} else if (deployments[0].status == 'STARTING') {
+					await message.send('_Successfully Updated!_');
+					await message.send('_Restarting..._')
+					clearInterval(intervalId);
+					await require('pm2').stop('hermit-md');
+				}
+             }, 5000);
+            await koyeb.reDeploy(config.KOYEB_APP_NAME);
         } catch (error) {
-            await message.send(`_Error during update check: ${error.message}_`);
+            await message.send(`_Error during update: ${error.message}_`);
         }
-        return;
-    }
-
-    const msg = await message.reply('_Checking for updates..._');
-
-    try {
-        const status = await git.fetch();
-        const diffSummary = await git.diffSummary(['HEAD', 'origin/main']);
-        if (diffSummary.total === 0) {
-            await msg.edit('_No new updates available._');
-            return;
-        }
-
-        await message.send('_Updating..._');
-
-        await koyeb.reDeploy(config.KOYEB_APP_NAME);
-
-        await message.send('_Successfully Updated!_');
-        
-        await require('pm2').stop('hermit-md');
-    } catch (error) {
-        await message.send(`_Error during update: ${error.message}_`);
     }
 });
-
 
 Function({
     pattern: 'setvar ?(.*)',
@@ -82,7 +96,7 @@ Function({
                 serviceName: config.KOYEB_APP_NAME
             });
             await message.send(`*_Successfully Set_* *${varKey}:${varValue}*\n_ReDeploying..._`);
-            await require('pm2').delete('all');
+            await require('pm2').stop('hermit-md');
         } catch (error) {
             await message.send(`Error: ${error.message}`);
         }
@@ -107,7 +121,7 @@ Function({
             serviceName: config.KOYEB_APP_NAME
         });
         await message.send(`*${match.toUpperCase()} Deleted*\n_ReDeploying..._`);
-        await require('pm2').delete('all');
+        await require('pm2').stop('hermit-md');
     } catch (error) {
         await message.send(`Error: ${error.message}`);
     }
