a69a16c1eee9e2c041f4db94a60e454d8b55008f
Adithyan <hermitx39@gmail.com>
2024-08-01 02:51:18 +0530

    Deprecation of heroku

--- plugins/koyeb.js
+++ plugins/koyeb.js
@@ -19,69 +19,12 @@ pm2.connect(function(err) {
         if (err) throw err;
     });
 });
+
 }
 
 if (config.KOYEB_API_KEY) {
 const Koyeb = require('node-koyeb-api');
 const koyeb = new Koyeb(config.KOYEB_API_KEY);
-	
-Function({
-    pattern: 'update ?(.*)',
-    fromMe: true,
-    desc: 'Update bot from GitHub repository',
-    type: 'koyeb'
-}, async (message, match, client) => {
-	if (!match || (match.toLowerCase() == 'check')) {
-        await git.fetch();
-        const commits = await git.log(['main..origin/main']);
-        let msg = ''; let no = 1;
-        commits.all.map((commit) => {
-        	msg += '' + no++ + '. ' + commit.message + '\n';
-        });
-        if (commits.total > 0) {
-            const interactiveMessage = {
-                title: 'New update available!\n\nChanges:',
-                text: msg,
-                footer: 'hermit-md',
-                subtitle: 'hermit-md',
-                buttons: [{
-                    type: 'button',
-                    display_text: 'Update Now',
-                    id: prefix + 'update now'
-                }]
-            };
-            
-            return await client.interactiveMessage(message.jid, interactiveMessage);
-        } else {
-            return await message.send('_Bot is completely up-to-date!_');
-        }
-     }
-
-    if (match && (match.toLowerCase() === 'now' || match.toLowerCase() === 'start')) {
-        try {
-            await message.send('_Build started_')
-            let intervalId;
-            intervalId = setInterval(async function() {
-            	const { deployments } = await koyeb.getDeployments(config.KOYEB_APP_NAME)
-                if (deployments[0].status == 'CANCELED') {
-                	await message.reply('*Deployment Canceled*')
-					clearInterval(intervalId);
-                } else if (deployments[0].status == 'STOPPED') {
-                	await message.reply('*Deployment Stopped*')
-	                clearInterval(intervalId);
-				} else if (deployments[0].status == 'STARTING') {
-					await message.send('_Successfully Updated!_');
-					await message.send('_Restarting..._')
-					clearInterval(intervalId);
-					await require('pm2').stop('hermit-md');
-				}
-             }, 5000);
-            await koyeb.reDeploy(config.KOYEB_APP_NAME);
-        } catch (error) {
-            await message.send(`_Error during update: ${error.message}_`);
-        }
-    }
-});
 
 Function({
     pattern: 'setvar ?(.*)',
@@ -168,4 +111,4 @@ Function({
     }
 });
 
-		    }
+}
