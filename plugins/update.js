28a4ee27fac08d267d58317d71873c2de60c1479
Adithyan R <93932522+A-d-i-t-h-y-a-n@users.noreply.github.com>
2025-01-05 16:12:31 +0530

    fix

--- plugins/update.js
+++ plugins/update.js
@@ -22,51 +22,43 @@ Function({
 			msg += '' + no++ + '. ' + commit.message + '\n';
 		});
 		if (commits.total > 0) {
-			const interactiveMessage = {
-				title: 'New update available!\n\nChanges:',
-				text: msg,
-				footer: 'hermit-md',
-				subtitle: 'hermit-md',
-				buttons: [{
-					type: 'button',
-					display_text: 'Update Now',
-					id: prefix + 'update now'
-				}]
-			};
-
-			return await message.client.interactiveMessage(message.jid, interactiveMessage);
+			await message.send(
+				`*New update available!*\n\n*Changes:*\n${msg}\n\nType '${prefix}update now' to update.`
+			);
 		} else {
-			return await message.send('_Bot is completely up-to-date!_');
+			await message.send('_Bot is completely up-to-date!_');
 		}
+		return;
 	}
 
 	if (match && (match.toLowerCase() === 'now' || match.toLowerCase() === 'start')) {
 		await git.fetch();
 		const branches = await git.branch(['-r']);
-        const remoteBranch = branches.all.find(branch => branch.includes('origin/main'));
-        if (!remoteBranch) return;
+		const remoteBranch = branches.all.find(branch => branch.includes('origin/main'));
+		if (!remoteBranch) return;
+
 		const commits = await git.log([`HEAD..${remoteBranch}`]);
-		if (!commits.total > 0) return await message.send('_Bot is completely up-to-date!_');
+		if (!commits.total > 0) {
+			return await message.send('_Bot is completely up-to-date!_');
+		}
+
 		if (config.KOYEB_API_KEY) {
 			const Koyeb = require('node-koyeb-api');
 			const koyeb = new Koyeb(config.KOYEB_API_KEY);
 
 			try {
-				await message.send('_Build started_')
+				await message.send('_Build started..._');
 				let intervalId;
-				intervalId = setInterval(async function() {
-					const {
-						deployments
-					} = await koyeb.getDeployments(config.KOYEB_APP_NAME)
+				intervalId = setInterval(async function () {
+					const { deployments } = await koyeb.getDeployments(config.KOYEB_APP_NAME);
 					if (deployments[0].status == 'CANCELED') {
-						await message.reply('*Deployment Canceled*')
+						await message.send('*Deployment Canceled*');
 						clearInterval(intervalId);
 					} else if (deployments[0].status == 'STOPPED') {
-						await message.reply('*Deployment Stopped*')
+						await message.send('*Deployment Stopped*');
 						clearInterval(intervalId);
 					} else if (deployments[0].status == 'STARTING') {
-						await message.send('_Successfully Updated!_');
-						await message.send('_Restarting..._')
+						await message.send('_Successfully Updated! Restarting..._');
 						clearInterval(intervalId);
 						await pm2.stop('hermit-md');
 					}
@@ -78,27 +70,30 @@ Function({
 		} else if (config.RENDER_API) {
 			const Render = require('../lib/render');
 			const render = new Render(config.RENDER_API, config.RENDER_NAME);
-			let intervalId;
-			intervalId = setInterval(async function() {
-				const deployment = await render.deployInfo('1');
-				if (deployment[0].deploy.status == 'canceled') {
-					await message.reply('*Deploy Cancelled*')
-					clearInterval(intervalId);
-				} else if (deployment[0].deploy.status == 'live') {
-					await message.send('_Successfully Updated!_');
-					await message.send('_Restarting..._')
-					clearInterval(intervalId);
-					await pm2.stop('hermit-md');
-				}
-			}, 5000);
-		   await render.deploy('clear');
-		   await message.send('_Build started_')
+
+			try {
+				await message.send('_Build started..._');
+				let intervalId;
+				intervalId = setInterval(async function () {
+					const deployment = await render.deployInfo('1');
+					if (deployment[0].deploy.status == 'canceled') {
+						await message.send('*Deploy Cancelled*');
+						clearInterval(intervalId);
+					} else if (deployment[0].deploy.status == 'live') {
+						await message.send('_Successfully Updated! Restarting..._');
+						clearInterval(intervalId);
+						await pm2.stop('hermit-md');
+					}
+				}, 5000);
+				await render.deploy('clear');
+			} catch (error) {
+				await message.send(`_Error during update: ${error.message}_`);
+			}
 		} else {
-			await git.reset('hard', ['HEAD'])
-			await git.pull()
-			await message.send('_Updated_')
-			await message.send('_Rebooting..._')
-			return await pm2.restart('hermit-md');
+			await git.reset('hard', ['HEAD']);
+			await git.pull();
+			await message.send('_Updated! Rebooting..._');
+			await pm2.restart('hermit-md');
 		}
 	}
 });
