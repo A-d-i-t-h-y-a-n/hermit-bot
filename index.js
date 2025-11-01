213800ad3d4bdffe88696eaf99cd45fc6bcbb597
Adithyan R <93932522+A-d-i-t-h-y-a-n@users.noreply.github.com>
2025-11-01 23:31:49 +0530

    baileys as global

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const client = require('./lib/client')
+let _baileys = null;
 
 const connect = async () => {
 	try {
+		if (!_baileys) {
+			_baileys = await import('baileys');
+			global.Baileys = _baileys;
+		}
 		await client.connect()
 	} catch (error) {
 		console.error(error)
 	}
 }
-
 connect()
